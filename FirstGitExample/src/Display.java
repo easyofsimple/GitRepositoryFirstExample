386ff611207f52ebe02ed358190d9ed5990557a8
user name <user@email.com>
2014-05-22 12:41:03 -0400

    Added Display.java, check in

---
+++ FirstGitExample/src/Display.java
@@ -0,0 +1,10 @@
+
+public class Display {
+	String data[] = {"git", "new", "branch"};
+	
+	public void display() {
+		for (int i=0; i< data.length;i++) {
+			System.out.println(data[i]);
+		}
+	}
+}
