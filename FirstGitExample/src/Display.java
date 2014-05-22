daa50fb13cbc1099cb95498ddc8ab625d705224e
user name <user@email.com>
2014-05-22 13:15:12 -0400

    Completed Display.java

--- FirstGitExample/src/Display.java
+++ FirstGitExample/src/Display.java
@@ -3,8 +3,9 @@ public class Display {
 	String data[] = {"git", "new", "branch"};
 	
 	public void display() {
+		System.out.println("String:");
 		for (int i=0; i< data.length;i++) {
-			System.out.println(data[i]);
+			System.out.println("String data: " + data[i]);
 		}
 	}
 }
