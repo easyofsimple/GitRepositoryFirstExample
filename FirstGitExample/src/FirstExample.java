bff2c7415a09871900d18ed7edf023d877c7de5b
user name <user@email.com>
2014-05-22 12:48:50 -0400

    added system out information.

--- FirstGitExample/src/FirstExample.java
+++ FirstGitExample/src/FirstExample.java
@@ -3,6 +3,11 @@ public class FirstExample {
 	public static void main(String [] args) {
 			System.out.println("First Example without checking in");
 			System.out.println("try the second check in");
+			
+			System.out.println("=============================================");
+			System.out.println("Hi, I have added Display.java");
+			System.out.println("Display.java displays strings");
+		
 	}
 
 }
